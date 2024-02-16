faed3dbc03b5348c1bcb4e86133f2e589ecc58a5
fernando-angeli <fernando_angeli@hotmail.com>
2024-02-16 17:34:39 -0300

    Update ResourceExceptionHandler

--- wt-client/src/main/java/com/wintech/wtclientservice/controllers/exceptions/ResourceExceptionHandler.java
+++ wt-client/src/main/java/com/wintech/wtclientservice/controllers/exceptions/ResourceExceptionHandler.java
@@ -3,9 +3,12 @@ package com.wintech.wtclientservice.controllers.exceptions;
 
 import com.wintech.wtclientservice.services.exceptions.DatabaseException;
 import com.wintech.wtclientservice.services.exceptions.ResourceNotFoundException;
+import com.wintech.wtuser.controllers.exceptions.ValidationError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -36,5 +39,20 @@ public class ResourceExceptionHandler {
         error.setMessage(e.getMessage());
         error.setPath(req.getRequestURI());
         return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        for(FieldError f : e.getBindingResult().getFieldErrors()){
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
     }
 }
